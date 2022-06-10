f32f1e016ab0153ab405922afc19847e8787ff4d
Sasha Honcharenko <san4os2012@gmail.com>
2022-06-10 12:46:47 +0000

    Deleted src/exchanger/classes/avaliable.py, src/exchanger/classes/exchange.py, src/exchanger/classes/exchanger_final.py, src/exchanger/classes/exchanger_uah.py, src/exchanger/classes/exchanger_usd.py, src/exchanger/const/available_currency.csv, src/exchanger/const/exchange_rate_data.csv, src/exchanger/main.py, src/exchanger/test.py

--- src/exchanger/classes/avaliable.py
+++
@@ -1,8 +0,0 @@
-class Avaliable:
-
-    def __init__(self, currency, available: float):
-        self.currency = currency
-        self.available = available
-
-    def __str__(self):
-        return f'{self.currency}{self.available}'
