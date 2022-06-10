c4ce7dd2d560787ee06f4c84dd2faa72a8e97329
Cubinec-py <Ujyxfhtyrj95>
2022-06-10 15:41:58 +0300

    hw15

---
+++ src/exchanger/classes/exchanger_uah.py
@@ -0,0 +1,45 @@
+import csv
+
+
+def update_avaliable_csv(address, new_value):
+
+    filepath = 'src/exchanger/const/available_currency.csv'
+    row_num = address
+
+    with open(filepath, 'r+t') as csvfile:
+        reader = csv.reader(csvfile)
+        lines = []
+
+        for current_line in reader:
+            if reader.line_num == row_num:
+                current_line[1] = new_value
+            lines.append(current_line)
+
+        csvfile.seek(0)
+        csv.writer(csvfile).writerows(lines)
+        csvfile.truncate()
+
+
+def print_result(a, result):
+    return print(f'\nUNAVAILABLE, REQUIRED BALANCE {a.currency} {round(result, 2)}, AVAILABLE {round(a.available, 2)}\n')
+
+
+class ExchangerUah:
+
+    def __init__(self, exchange, avaliables):
+        self.exchanges = exchange
+        self.avaliables = avaliables
+
+    def exchanger_uah(self, exchange, avaliables, val):
+        for i in exchange:
+            for a in avaliables:
+                if a.available >= (float(val[1]) * i.buy) and a.currency != val[0]:
+                    result = float(val[1]) * i.buy
+                    new_balance = float(a.available) - float(result)
+                    update_avaliable_csv(address=2, new_value=f'{new_balance}')
+                    print(f'\n{a.currency} {round(result, 2)}, RATE {round(i.buy, 2)}\n')
+                    break
+                elif a.currency != val[0]:
+                    result = float(val[1]) * i.buy
+                    print_result(a, result)
+                    break
